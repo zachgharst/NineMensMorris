cb2dedb2a32d6eea56606224e3f81550180ceaa8
zach.gharst <zdgharst@gmail.com>
2020-10-08 18:22:46 -0500

    Add ability to place men on board. -New primitive functions for placing men; no logic behind decision. -Create man class. -Modify board image. -Choose more informative naming conventions.

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-enum Cell { Invalid, Vacant, White, Black };
+public enum Cell { Invalid, Vacant, White, Black };
 
-public class Board : MonoBehaviour
+public class BoardManager : MonoBehaviour
 {
     public Sprite sprite;
+    public static int turn = 0;
 
-    Cell[,] BoardState = {
+    public static Cell[,] BoardState = {
         {  Cell.Vacant, Cell.Invalid, Cell.Invalid,  Cell.Vacant, Cell.Invalid, Cell.Invalid,  Cell.Vacant },
         { Cell.Invalid,  Cell.Vacant, Cell.Invalid,  Cell.Vacant, Cell.Invalid,  Cell.Vacant, Cell.Invalid },
         { Cell.Invalid, Cell.Invalid,  Cell.Vacant,  Cell.Vacant,  Cell.Vacant, Cell.Invalid, Cell.Invalid },
@@ -25,31 +26,34 @@ public class Board : MonoBehaviour
             for (int j = 0; j < 7; j++)
             {
                 if (BoardState[i, j] != Cell.Invalid)
-                    SpawnTile(i, j, BoardState[i, j]);
+                    DrawTile(i, j, BoardState[i, j]);
             }
         }
     }
 
-    private void SpawnTile(int x, int y, Cell status)
+    private void DrawTile(int x, int y, Cell status)
     {
         GameObject g = new GameObject((char)(x + 97) + "" + (y + 1));
-        g.transform.position = new Vector2(x, y);
+        g.transform.position = new Vector2(x - 3, y - 3);
+        g.transform.SetParent(this.transform);
+        g.AddComponent(typeof(Man));
+        g.AddComponent<BoxCollider>();
         var s = g.AddComponent<SpriteRenderer>();
         s.sprite = sprite;
 
-        if (status == Cell.Vacant)
+        if (status == Cell.White)
         {
-            s.color = new Color(0, 0, 255);
-        }
-
-        else if (status == Cell.White)
-        {
-            s.color = new Color(255, 255, 255);
+            s.color = new Color(255, 255, 255, 1);
         }
 
         else if (status == Cell.Black)
         {
-            s.color = new Color(0, 0, 0);
+            s.color = new Color(0, 0, 0, 1);
+        }
+
+        else
+        {
+            s.color = new Color(0, 0, 0, 0);
         }
     }
 }
---
+++ Assets/Scripts/Man.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Man : MonoBehaviour
+{
+    int i = 0;
+    int j = 0;
+
+    void OnMouseDown()
+    {
+        var s = gameObject.GetComponent<SpriteRenderer>();
+
+        if (BoardManager.turn == 0)
+        {
+            BoardManager.BoardState[i, j] = Cell.White;
+            s.color = new Color(255, 255, 255);
+        }
+        else
+        {
+            BoardManager.BoardState[i, j] = Cell.Black;
+            s.color = new Color(0, 0, 0);
+        }
+
+        BoardManager.turn = (BoardManager.turn + 1) % 2;
+    }
+}
