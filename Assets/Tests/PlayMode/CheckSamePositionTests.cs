0f94de827e1172a753e3b70923c25503caacc149
Thomas-Tran-UMKC <ttnn3@mail.umkc.edu>
2020-11-19 06:04:37 -0600

    CheckSamePosition test
    Unit test for CheckSamePosition()

---
+++ Assets/Tests/PlayMode/CheckSamePositionTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CheckSamePositionTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            GameObject g = new GameObject("BoardManager");
+            g.AddComponent<BoardManager>();
+            BoardManager.InitGame();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            BoardManager.ResetBoard();
+        }
+
+        [Test]
+        public void CheckSamePositionTestValidInput()
+        {
+            int targetRow = 6;
+            int targetCol = 6;
+            int sourceRow = 6;
+            int sourceCol = 6;
+            Assert.IsTrue(BoardManager.CheckSamePosition(sourceRow, targetRow, sourceCol, targetCol));
+        }
+
+        [Test]
+        public void CheckSamePositionTestNotSameNode()
+        {
+            int targetRow = 3;
+            int targetCol = 6;
+            int sourceRow = 0;
+            int sourceCol = 6;
+            Assert.IsFalse(BoardManager.CheckSamePosition(sourceRow, targetRow, sourceCol, targetCol));
+        }
+    }
+}
