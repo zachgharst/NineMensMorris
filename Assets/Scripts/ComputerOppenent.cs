a337c03f418510e6ef21844d3bcc464e286172f8
Thomas Yang <smartcow831@gmail.com>
2020-12-02 23:56:17 -0600

    "AI" Piece Placement

---
+++ Assets/Scripts/ComputerOppenent.cs
@@ -0,0 +1,27 @@
+﻿/*
+ *  UMKC CS 449: Nine Men's Morris implementation
+ *  Copyright (C) 2020 Forgetful Wanderers
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  https://github.com/ZDGharst/UMKC_ForgetfulWanderers/
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerOppenent : MonoBehaviour
+{
+
+    
+
+}
