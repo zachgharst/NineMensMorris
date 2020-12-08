a4b65832b12866e4386af71aa9b1f1d362c670db
Thomas-Tran-UMKC <ttnn3@mail.umkc.edu>
2020-12-07 19:14:19 -0600

    Title Screen
    New title scene. Adding '1 Player' and '2 Players' buttons.

---
+++ Assets/Scripts/TitleSceneManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneManager : MonoBehaviour
+{
+    public Button onePlayerButton;
+    public Button twoPlayerButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(1)
+    }
+}
